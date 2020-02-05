b006a887c131943febc810a89a6384f9f5d8f429
Arun Upadhyay <arunnnet@gmail.com>
2020-02-04 21:53:09 -0500

    Simple change to post

--- crud-op/src/components/Posts.vue
+++ crud-op/src/components/Posts.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
         <h1>Posts</h1>
+        <span>Total Post <b>{{totalPost}}</b></span>
         <div v-bind:key="post._id" v-for="post in posts">
             <Post v-bind:post="post"></Post>
         </div>
@@ -18,7 +19,7 @@
         },
         computed: {
             totalPost: function () {
-
+                return this.posts.length;
             }
         }
     }
