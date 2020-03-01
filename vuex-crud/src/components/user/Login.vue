df955d9bcf5ade7c2a23d394d86e253721df5636
Arun Upadhyay <arunnnet@gmail.com>
2020-03-01 13:22:03 -0500

    Validation in login page

--- vuex-crud/src/components/user/Login.vue
+++ vuex-crud/src/components/user/Login.vue
@@ -1,5 +1,5 @@
 <template>
-    <div v-show="!$store.getters.isAuthenticated" class="login">
+    <div v-show="!$store.getters.isAuthenticated" class="login" v-cloak>
 
         <form @submit.prevent="login">
             <div class="form-group">
@@ -18,6 +18,12 @@
             <button type="submit" class="btn btn-primary" style="margin-right: 20px">Submit</button>
             <button type="button" class="btn btn-primary" @click="register">Register</button>
         </form>
+        <div v-if="errors.length" class="error-div">
+            <b>Please correct the following errors</b>
+            <ul>
+                <li :key="error.id" v-for="error in errors" class="error.id">{{error.text}}</li>
+            </ul>
+        </div>
     </div>
 </template>
 
@@ -27,7 +33,8 @@
         data() {
             return {
                 username: '',
-                password: ''
+                password: '',
+                errors: []
             }
         },
         created() {
@@ -42,16 +49,33 @@
         },
         methods: {
             login: function (event) {
+                this.validation();
+                // validate form.
+                if (this.errors.length !== 0) {
+                    return false;
+                }
                 event.preventDefault();
                 this.$store.dispatch(('findUser'), {
                     username: this.username,
                     password: this.password
                 }).then(() => {
                     this.$router.push({name: 'posts'})
+                }).catch(err => {
+                    this.errors.push({id: 'error1', text: 'Invalid username or password.'});
+                    console.log(err);
                 });
             },
             register: function () {
                 this.$router.push({name: 'register'})
+            },
+            validation: function () {
+                this.errors = [];
+                if (!this.username) {
+                    this.errors.push({id: 'error1', text: 'Username required.'});
+                }
+                if (!this.password) {
+                    this.errors.push({id: 'error2', text: 'Password required.'});
+                }
             }
 
         }
@@ -62,4 +86,13 @@
     .login {
         margin: 30px;
     }
+
+    .error-div ul li {
+        color: red;
+    }
+
+    [v-cloak] {
+        display: none
+    }
+
 </style>
